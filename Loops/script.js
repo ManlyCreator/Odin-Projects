88f629906ca74c826efc9e12d160ee5e268b9c8b
Rami <myersrami17@gmail.com>
2025-07-11 09:04:50 -0500

    Finished TOP excercises from javascript.info

--- Loops/script.js
+++ Loops/script.js
@@ -1,7 +1,13 @@
-// TODO: Write the sumOfTripledEvens(array) function as described in TOP - Loops & Arrays
-
 main();
 
 function main() {
+    arr = [ 1, 2, 3, 4, 5 ];
+    console.log(sumOfTripledEvens(arr));
+}
 
+function sumOfTripledEvens(arr) {
+    return arr
+    .filter((n) => { return n % 2 == 0; })
+    .map((n) => { return n * 3; })
+    .reduce((total, n) => { return total + n; }, 0);
 }
