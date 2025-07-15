4310ad3cc852470a913b1e3058f7539d0b9875ca
Rami <myersrami17@gmail.com>
2025-07-14 20:37:53 -0500

    Calculator now uses two stacks to evaluate expression, added TODO for more features

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,22 @@
-// TODO: Evaluate equation in the order that it is input
-// TODO: Incorporate an algorithm to apply PEMDAS to the equation
+// TODO: Backspace
+// TODO: Floats
+// TODO: Keyboard Input
 
 const screenText = document.querySelector(".screen-text")
+const clearButton = document.querySelector(".other-buttons .clear")
+const deleteButton = document.querySelector(".other-buttons .delete")
 const buttons = document.querySelectorAll(".buttons button")
 
 const operands = "0123456789"
 const operators = "+-*/="
 
+let justOperated = false
+
 let operandStack = []
 let operatorStack = []
+
+clearButton.addEventListener("click", clear)
+deleteButton.addEventListener("click", backspace)
 
 buttons.forEach((button) => {
     button.addEventListener("click", handleInput)
@@ -16,24 +24,64 @@ buttons.forEach((button) => {
 
 function handleInput(event) {
     const command = event.target.innerText
+    console.log("Operand Stack:", operandStack)
+    console.log("Operator Stack:", operatorStack)
     if (command == "=") {
         if (operands.includes(screenText.innerText))
             operandStack.push(Number(screenText.innerText))
-        console.log(operandStack)
-        console.log(operatorStack)
+        screenText.innerText = evaluate()
     } else if (operands.includes(command)) {
-        if (operators.includes(screenText.innerText))
+        if (!operands.includes(screenText.innerText))
             screenText.innerText = command
         else
             screenText.innerText += command
-    // TODO: Evaluate the expression if operand stack is full to avoid errors
-    } else if (operators.includes(command) && operandStack.length < 1) {
-        operandStack.push(Number(screenText.innerText))
-        operatorStack.push(command)
-        screenText.innerText = command
+    } else if (operators.includes(command)) {
+        if (!justOperated) operandStack.push(Number(screenText.innerText))
+        justOperated = false
+        if (operandStack.length == 2) {
+            screenText.innerText = evaluate()
+        } else {
+            operatorStack.push(command)
+            screenText.innerText = command
+        }
     }
 }
 
 function evaluate() {
-    // TODO: Evaluate expression using the top-two operators on the stack with the top operand, then clear both stacks
+    const y = operandStack.pop()
+    const x = operandStack.pop()
+    const operator = operatorStack.pop()
+    console.log("X:", x)
+    console.log("Y:", y)
+    console.log("Operator:", operator)
+    let result
+    switch (operator) {
+        case "+":
+            result = x + y
+            break
+        case "-":
+            result = x - y
+            break
+        case "*":
+            result = x * y
+            break
+        case "/":
+            result = x / y
+            break
+    }
+    if (result != undefined && result != NaN)
+        operandStack.push(result)
+    justOperated = true
+    return result
+}
+
+function clear() {
+    operandStack = []
+    operatorStack = []
+    screenText.innerText = ""
+    justOperated = false
+}
+
+function backspace() {
+
 }
