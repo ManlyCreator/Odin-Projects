f009b10f3ee194679acbd87a824e0a5879a3ecfa
Rami <myersrami17@gmail.com>
2025-07-16 13:42:38 -0500

    Backspace functional, input & evaluation logic functional, implementing floats

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,5 +1,4 @@
-// TODO: Backspace
-// TODO: Floats
+// TODO: Debug why str.includes() does not pick up the "." symbol
 // TODO: Keyboard Input
 
 const screenText = document.querySelector(".screen-text")
@@ -7,13 +6,15 @@ const clearButton = document.querySelector(".other-buttons .clear")
 const deleteButton = document.querySelector(".other-buttons .delete")
 const buttons = document.querySelectorAll(".buttons button")
 
-const operands = "0123456789"
-const operators = "+-*/="
+const operands = "0123456789."
+const operators = "+-*/"
 
 let justOperated = false
+let error = false
 
+let operator = ""
 let operandStack = []
-let operatorStack = []
+let operandPointer = 0
 
 clearButton.addEventListener("click", clear)
 deleteButton.addEventListener("click", backspace)
@@ -23,38 +24,47 @@ buttons.forEach((button) => {
 })
 
 function handleInput(event) {
+    if (error) clear()
     const command = event.target.innerText
-    console.log("Operand Stack:", operandStack)
-    console.log("Operator Stack:", operatorStack)
-    if (command == "=") {
-        if (operands.includes(screenText.innerText))
-            operandStack.push(Number(screenText.innerText))
+    console.log(screenText.innerText.includes("."))
+    if (command == "=" && operandStack.length == 2) {
         screenText.innerText = evaluate()
+        operandPointer = 0
+        
     } else if (operands.includes(command)) {
-        if (!operands.includes(screenText.innerText))
+        if (command == "." && !screenText.innerText.includes(".")) {
+            screenText.innerText += "."
+        } else if (isNaN(Number(screenText.innerText)) || justOperated == true) {
             screenText.innerText = command
-        else
+            justOperated = false
+        } else 
             screenText.innerText += command
-    } else if (operators.includes(command)) {
-        if (!justOperated) operandStack.push(Number(screenText.innerText))
-        justOperated = false
-        if (operandStack.length == 2) {
-            screenText.innerText = evaluate()
-        } else {
-            operatorStack.push(command)
-            screenText.innerText = command
-        }
+        operandStack[operandPointer] = Number(screenText.innerText)
+    } else if (operators.includes(command) && operandStack.length > 0) {
+        operandPointer++
+        screenText.innerText = command
+        if (operandStack.length == 2) screenText.innerText = evaluate(); operandPointer = 1
+        operator = command
     }
+    console.log("Screen Text:", screenText.innerText)
+    console.log("Just Operated:", justOperated)
+    console.log("Operand Stack:", operandStack)
+    console.log("Operand Pointer:", operandPointer)
+    console.log("Operator:", operator)
 }
 
 function evaluate() {
     const y = operandStack.pop()
+
+    if (y == 0) {
+        error = true
+        return "No division by zero! >:("
+    }
+
     const x = operandStack.pop()
-    const operator = operatorStack.pop()
-    console.log("X:", x)
-    console.log("Y:", y)
-    console.log("Operator:", operator)
+
     let result
+
     switch (operator) {
         case "+":
             result = x + y
@@ -69,19 +79,30 @@ function evaluate() {
             result = x / y
             break
     }
-    if (result != undefined && result != NaN)
-        operandStack.push(result)
+
+    operandStack[0] = result
     justOperated = true
     return result
 }
 
 function clear() {
     operandStack = []
-    operatorStack = []
+    operator = ""
     screenText.innerText = ""
     justOperated = false
+    operandPointer = 0
+    error = false
 }
 
 function backspace() {
-
+    if (isNaN(screenText.innerText)) return 
+    let textCopy = screenText.innerText.split("")
+    textCopy.pop()
+    textCopy = textCopy.join("")
+    screenText.innerText = textCopy
+    if (textCopy.length > 0) {
+        operandStack[operandPointer] = Number(textCopy)
+    } else {
+        clear()
+    }
 }
